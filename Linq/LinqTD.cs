a62af21244e1a3476a26a39876a8828efbc42cf9
sedar-creator <153775753+sedar-creator@users.noreply.github.com>
2024-04-17 11:58:39 +0200

    solution

--- Linq/LinqTD.cs
+++ Linq/LinqTD.cs
@@ -5,56 +5,67 @@
 		 * lettre fournie en parametre
 		 */
         public static IEnumerable<string> FilterElementsBeginningWithSpecificLetter(IEnumerable<string> sequence, char letter) {
-            throw new NotImplementedException();
+            return sequence.Where(x => x.StartsWith(letter));
         }
 
         /**
 		 * Cette fonction doit retourner le premier element de la séquence de la taille fournie en parametre
 		 */
-        public static string? FirstElementOfLengthI(IEnumerable<string> sequence, int size) {
-            throw new NotImplementedException();
+        public static string? FirstElementOfLengthI(IEnumerable<string> sequence, int size){
+            return sequence.FirstOrDefault(x => x.Length == size);
         }
 
         /**
 		 * Cette fonction doit retourner la moyenne de la séquence fournie en paramètre
 		 */
         public static double AverageOfSequence(IEnumerable<int> intSequence) {
-            throw new NotImplementedException();
+            return intSequence.Average();
         }
 
         /**
 		 * Cette fonction doit retourner la moyenne des éléments uniques (sans les duplications) de la séquence fournie en paramètre
 		 */
         public static double AverageOfUniqueSequenceElements(IEnumerable<int> intSequence) {
-            throw new NotImplementedException();
+            return intSequence.Distinct().Average();
         }
 
         /**
 		 * Cette fonction doit retourner un tuple (min, max) de la séquence
 		 */
         public static (int, int) MinMaxOfSequence(IEnumerable<int> intSequence) {
-            throw new NotImplementedException();
+            return (
+                intSequence.Min(), 
+                intSequence.Max()
+                );
         }
 
         /**
 		 * Cette fonction doit retourner la liste des ids des objets de la séquence
 		 */
         public static IEnumerable<int> IdsListOfSequence(IEnumerable<DummyModel> objectSequence) {
-            throw new NotImplementedException();
+            return objectSequence.Select(x => x.Id);
         }
 
         /**
 		 * Cette fonction retourne l'age moyen dans la séquence pour les objets non supprimés
 		 */
         public static double AgeAverageOfSequence(IEnumerable<DummyModel> objectSequence) {
-            throw new NotImplementedException();
+           /* return objectSequence
+                .Where(x => !x.IsDeleted)
+                .Select(x => x.Age )
+                .Average();*/
+            
+            return objectSequence
+                .Where(x => !x.IsDeleted)
+                .Average(x => x.Age);
         }
 
         /**
 		 * Cette fonction retourne vrai si l'un des membres dans la séquence a un age egal à celui donné en paramètres
 		 */
-        public static bool DoesSomeonesHasASpecificAge(IEnumerable<DummyModel> objectSequence, int age) {
-            throw new NotImplementedException();
+        public static bool DoesSomeonesHasASpecificAge(IEnumerable<DummyModel> objectSequence, int age){
+            //return objectSequence.FirstOrDefault(x => x.Age == age) != null;
+            return objectSequence.Any(x => x.Age == age);
         }
     }
 }
